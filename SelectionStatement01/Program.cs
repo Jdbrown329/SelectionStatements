5341447c8b4e35113850806f9b5d292ff4416cde
Jordan Brown <j.d.brown329@gmail.com>
2022-09-23 09:38:22 -0500

    first commit

---
+++ SelectionStatement01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatements01
+{
+    class program
+    {
+        static void Main(string[]  args)
+        {
+            Console.WriteLine("Guess what my favorite number is:");
+            int faveN = 33;
+            
+            int userInput= int.Parse( Console.ReadLine() );
+
+            if (userInput > faveN)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else if (userInput < faveN)
+            {
+                Console.WriteLine("Too low");
+            }
+
+            else if (userInput == faveN)
+            {
+                Console.WriteLine("Perfect, that's my fave number. GOOD JOB!" );
+            }
+         
+
+        }
+    }
+
+}
